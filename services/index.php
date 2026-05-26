2be45b451208af51780958a9649d43e37bbfaa3a
Zambelli <zambelli@gmail.com>
2026-05-26 16:00:21 -0300

    controle de 404 nos diretórios

--- services/index.php
+++ services/index.php
@@ -1,5 +1,4 @@
-<?php /** @var array $t */ ?>
-<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/car-server.php';?>
-<?php include_once CAR_ROOT_WEB . '/config.inc';?>
-<?php include CAR_ROOT_WEB . '/lang/lang.inc'; ?>
-<?php car_redirect(CAR_PATH_WEB . '/' . $t['lang'] . '/');
+<?php
+http_response_code(404);
+include_once $_SERVER['DOCUMENT_ROOT'] . '/common/404.php';
+exit;
